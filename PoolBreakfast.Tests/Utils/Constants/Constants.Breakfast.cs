5d2a4c0141a43d659823a776d4b0bea8b074695d
mehrozdurrani <mehrozdurrani@gmail.com>
2023-09-21 09:53:06 +0200

    Code refactoring

--- PoolBreakfast.Tests/Utils/Constants/Constants.Breakfast.cs
+++ PoolBreakfast.Tests/Utils/Constants/Constants.Breakfast.cs
@@ -7,17 +7,18 @@ namespace PoolBreakfast.Tests.Utils.Constants
             public static readonly Guid Id = Guid.NewGuid();
             public const string Name = "Test Menu Name";
             public const string Description = "Test Menu Description";
-
             public static readonly DateTime StartDateTime = DateTime.UtcNow;
             public static readonly DateTime EndDateTime = DateTime.UtcNow;
             public static readonly DateTime LastModifiedDateTime = DateTime.UtcNow;
-            public static readonly List<string> Savory = new List<string>{
+            public static readonly List<string> Savory = new List<string>
+            {
                 "Test Oatmeal",
                 "Test Avocado Toast",
                 "Test Omelette",
                 "Test Salad"
             };
-            public static readonly List<string> Sweet = new List<string>{
+            public static readonly List<string> Sweet = new List<string>
+            {
                 "Test Oatmeal",
                 "Test Avocado Toast",
                 "Test Omelette",
