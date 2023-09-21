37e213c253eb6a35242618c3fdcc1f89bc9b1ac2
mehrozdurrani <mehrozdurrani@gmail.com>
2023-09-21 09:47:20 +0200

    Competed the Unit Testing of the Breakfast Service

--- PoolBreakfast.Tests/BreakfastServiceTest.cs
+++ PoolBreakfast.Tests/BreakfastServiceTest.cs
@@ -8,10 +8,12 @@ namespace PoolBreakfast.Tests
     public class BreakfastServiceTest
     {
         private readonly IBreakfastService _breakfastService;
+
         public BreakfastServiceTest()
         {
             _breakfastService = new BreakfastService();
         }
+
         [Fact]
         public void CreateBreakfastRequest_WhenRequestIsValid_ShouldReturnResultIsCreated()
         {
@@ -24,21 +26,50 @@ namespace PoolBreakfast.Tests
             // Assert
             testServiceResult.ShouldNotBeOfType<Error>();
             testServiceResult.ShouldBe(Result.Created);
-
         }
+
+        [Fact]
+        public void GetBreakfastRequest_WhenRequestIsValid_ShouldReturnResultIsBreakfast()
+        {
+            // Arrange
+            var breakfast = BreakfastServiceTestUtils.CreateBreakfast();
+            _ = _breakfastService.CreateBreakfast(breakfast);
+
+            // Act
+            var testServiceResult = _breakfastService.GetBreakfast(breakfast.Id);
+
+            // Assert
+            testServiceResult.Value.ShouldNotBeOfType<Error>();
+            testServiceResult.Value.ShouldBe(breakfast);
+        }
+
         [Fact]
         public void UpsertBreakfastRequest_WhenRequestIsValid_ShouldReturnResultIsNewlyCreated()
         {
             // Arrange
             var breakfast = BreakfastServiceTestUtils.CreateBreakfast();
-            _ = _breakfastService.CreateBreakfast(breakfast).Value;
+            _ = _breakfastService.CreateBreakfast(breakfast);
 
             // Act
             var testServiceResult = _breakfastService.UpdateBreakfast(breakfast.Id, breakfast);
 
             // Assert
             testServiceResult.Value.IsNewlyCreated.ShouldBeTrue();
+        }
 
+        [Fact]
+        public void DeleteBreakfastRequest_WhenRequestIsValid_ShouldReturnResultIsDeleted()
+        {
+            // Arrange
+            var breakfast = BreakfastServiceTestUtils.CreateBreakfast();
+            _ = _breakfastService.CreateBreakfast(breakfast);
+
+            // Act
+            var testServiceResult = _breakfastService.DeleteBreakfast(breakfast.Id);
+
+            // Assert
+            testServiceResult.ShouldNotBeOfType<Error>();
+            testServiceResult.ShouldBe(Result.Deleted);
         }
     }
 }
